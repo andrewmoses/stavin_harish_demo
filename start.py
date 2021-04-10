f4e9e17493ff2622dc5c905b18e63cd30496ffd6
Gates XII <andrew2moses@gmail.com>
2021-04-10 21:56:34 +0530

    route variable

--- start.py
+++ start.py
@@ -17,12 +17,26 @@ def hello_world():
    mycursor = mydb.cursor()
    mycursor.execute("SELECT * FROM tb1")
    myresult = mycursor.fetchall()
+   # myresult=[("val","val"),("val","val")]
    return render_template("index.html", myresult=myresult)
    # return "hello world"
 
 @app.route('/biblerc')
 def biblerc():
    return "i am in bible rc"
+
+@app.route('/product/<name>')
+def get_product(name):
+   mydb = mysql.connector.connect(
+   host="localhost",
+   user="root",
+   password="",
+   database="test"
+   )
+   mycursor = mydb.cursor()
+   mycursor.execute("""SELECT * FROM tb1 where pk=%s""" % (int(name)))
+   myresult = mycursor.fetchall()
+   return render_template("article.html", myresult=myresult)
 
 
 if __name__ == '__main__':
